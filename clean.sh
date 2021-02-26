28b36355f87949adb3ff1186abe5840b6e67b1ba
Jeremy Huddleston Sequoia <jeremyhu@apple.com>
2021-02-25 23:09:55 -0800

    clean.sh: Remove Sparkle build products
    Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>

--- clean.sh
+++ clean.sh
@@ -3,5 +3,6 @@ sudo rm -rf products
 rm -rf XQuartz-*.d*
 git submodule foreach --recursive git checkout -f
 git submodule foreach --recursive git clean -d -f
+sudo rm -rf src/Sparkle/build
 find . -name applied -exec rm {} \;
 git clean -d -f
