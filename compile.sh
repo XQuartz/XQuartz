6cc0f504117e2e2c638dc9c2b381cc5d93b883d2
Jeremy Huddleston Sequoia <jeremyhu@apple.com>
2021-03-05 22:19:25 -0800

    Create a shadow of /System/Library/Fonts, so we can index it
    Fixes: https://github.com/XQuartz/XQuartz/issues/99
    Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>

--- compile.sh
+++ compile.sh
@@ -144,7 +144,7 @@ export PS2PDF=${BUILD_TOOLS_PREFIX}/bin/ps2pdf
 
 export PATH="${PREFIX}/bin:${BUILD_TOOLS_PREFIX}/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 export PKG_CONFIG_PATH="${PREFIX}/share/pkgconfig:${PREFIX}/lib/pkgconfig"
-export FONTPATH="${PREFIX}/share/fonts/misc/,${PREFIX}/share/fonts/TTF/,${PREFIX}/share/fonts/OTF,${PREFIX}/share/fonts/Type1/,${PREFIX}/share/fonts/75dpi/:unscaled,${PREFIX}/share/fonts/100dpi/:unscaled,${PREFIX}/share/fonts/75dpi/,${PREFIX}/share/fonts/100dpi/,/Library/Fonts,/System/Library/Fonts"
+export FONTPATH="${PREFIX}/share/fonts/misc/,${PREFIX}/share/fonts/TTF/,${PREFIX}/share/fonts/OTF,${PREFIX}/share/fonts/Type1/,${PREFIX}/share/fonts/75dpi/:unscaled,${PREFIX}/share/fonts/100dpi/:unscaled,${PREFIX}/share/fonts/75dpi/,${PREFIX}/share/fonts/100dpi/,/Library/Fonts,${PREFIX}/share/system_fonts"
 export ACLOCAL="aclocal -I ${PREFIX}/share/aclocal -I ${BUILD_TOOLS_PREFIX}/share/aclocal"
 
 die() {
