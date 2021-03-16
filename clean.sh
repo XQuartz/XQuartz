db636fee935a1ccb7c023330adfc0a36b6f43560
Jeremy Huddleston Sequoia <jeremyhu@apple.com>
2021-03-15 17:44:10 -0700

    Update clean script to delete mesa build directories
    Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>

--- clean.sh
+++ clean.sh
@@ -4,5 +4,6 @@ rm -rf XQuartz-*.d*
 git submodule foreach --recursive git checkout -f
 git submodule foreach --recursive git clean -d -f
 sudo rm -rf src/Sparkle/build
+sudo rm -rf src/mesa/mesa/build.*
 find . -name applied -exec rm {} \;
 git clean -d -f
