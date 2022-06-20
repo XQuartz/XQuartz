1213df185dade9da775ba4cedcb32b545cc632a1
Jeremy Huddleston Sequoia <jeremyhu@apple.com>
2022-06-19 22:42:20 -0700

    xinit: Ensure that /System/Library/Fonts/Supplemental is included in the font path if present
    Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>

--- base/opt/X11/etc/X11/xinit/xinitrc.d/10-fontdir.sh
+++ base/opt/X11/etc/X11/xinit/xinitrc.d/10-fontdir.sh
@@ -1,10 +1,27 @@
 if [ -x /opt/X11/bin/xset ] ; then
-        fontpath="/opt/X11/share/fonts/misc/,/opt/X11/share/fonts/TTF/,/opt/X11/share/fonts/OTF,/opt/X11/share/fonts/Type1/,/opt/X11/share/fonts/75dpi/:unscaled,/opt/X11/share/fonts/100dpi/:unscaled,/opt/X11/share/fonts/75dpi/,/opt/X11/share/fonts/100dpi/,/opt/X11/share/system_fonts/"
+    fontpath="/opt/X11/share/fonts/misc/,/opt/X11/share/fonts/TTF/,/opt/X11/share/fonts/OTF,/opt/X11/share/fonts/Type1/,/opt/X11/share/fonts/75dpi/:unscaled,/opt/X11/share/fonts/100dpi/:unscaled,/opt/X11/share/fonts/75dpi/,/opt/X11/share/fonts/100dpi/"
 
-        [ -e "$HOME"/.fonts/fonts.dir ] && fontpath="$fontpath,$HOME/.fonts"
-        [ -e "$HOME"/Library/Fonts/fonts.dir ] && fontpath="$fontpath,$HOME/Library/Fonts"
-        [ -e /Library/Fonts/fonts.dir ] && fontpath="$fontpath,/Library/Fonts"
+    fontpath="${fontpath},/opt/X11/share/system_fonts/"
+    for subdir in /opt/X11/share/system_fonts/*/ ; do
+        [ -e "${subdir}"/fonts.dir ] && fontpath="${fontpath},${subdir}"
+    done
 
-        /opt/X11/bin/xset fp= "$fontpath"
-        unset fontpath
+    [ -e "${HOME}"/.fonts/fonts.dir ] && fontpath="${fontpath},${HOME}/.fonts"
+
+    if [ -e /Library/Fonts/fonts.dir ] ; then
+        fontpath="${fontpath},/Library/Fonts"
+        for subdir in /Library/Fonts/*/ ; do
+            [ -e "${subdir}"/fonts.dir ] && fontpath="${fontpath},${subdir}"
+        done
+    fi
+
+    if [ -e "${HOME}"/Library/Fonts/fonts.dir ] ; then
+        fontpath="$fontpath,${HOME}/Library/Fonts"
+        for subdir in "${HOME}"/Library/Fonts/*/ ; do
+            [ -e "${subdir}"/fonts.dir ] && fontpath="${fontpath},${subdir}"
+        done
+    fi
+
+    /opt/X11/bin/xset fp= "$fontpath"
+    unset fontpath
 fi
