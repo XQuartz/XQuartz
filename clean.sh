b32b32d20b600c526483bcfb944c90db26957b80
Jeremy Huddleston Sequoia <jeremyhu@apple.com>
2022-06-20 22:09:38 -0700

    clean.sh: Improve cleaning for meson projects
    Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>

--- clean.sh
+++ clean.sh
@@ -1,9 +1,9 @@
 sudo rm -rf /Applications/Utilities/XQuartz.app /opt/X11
 sudo rm -rf products
 rm -rf XQuartz-*.d*
-git submodule foreach --recursive git checkout -f
-git submodule foreach --recursive git clean -d -f
+git submodule foreach --recursive "git checkout -f"
+git submodule foreach --recursive "rm -rf build.arm64 build.x86_64 build.i386"
+git submodule foreach --recursive "git clean -d -f"
 sudo rm -rf src/Sparkle/build
-sudo rm -rf src/mesa/mesa/build.*
 find . -name applied -exec rm {} \;
 git clean -d -f
